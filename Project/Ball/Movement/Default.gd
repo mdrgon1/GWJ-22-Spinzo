1c399a16c1c80145116f0daa6c3fc840e65e7514
mdrgon1 <mdrgon1@umbc.edu>
2020-06-14 22:56:02 -0400

    added throwing state
    ball follows it's trajectory until it's either called back or it goes a certain distance

--- Project/Ball/Movement/Default.gd
+++ Project/Ball/Movement/Default.gd
@@ -1,13 +1,33 @@
 extends State
 
-var target_radius = 10
+const MAX_RADIUS = 25
+const MIN_RADIUS = 1
+const RADIUS_LERP = 1
+const MAX_RPS = 1.2
+const MIN_RPS = 0.3
+const RPS_LERP = 0.6
 
-func run(_delta):
-	rotate(_delta)
+var target_radius : float
+var rotations_per_sec : float
+
+func enter(_args):
+	target_radius = MAX_RADIUS
+	rotations_per_sec = MIN_RPS
+
+func run(delta):
+	if(Input.is_action_pressed("throw_ball")):
+		target_radius += (MIN_RADIUS - target_radius) * RADIUS_LERP * delta
+		rotations_per_sec += (MAX_RPS - rotations_per_sec) * RPS_LERP * delta
+	
+	if(Input.is_action_just_released("throw_ball")):
+		return "Throwing"
+	
+	print(target_radius, ", ", rotations_per_sec)
+	
+	rotate(delta)
 
 # rotate the ball around player like normal
-func rotate(_delta):	
-	var rotations_per_sec = 0.5
+func rotate(delta):
 	var tangent_velocity = (owner.to_player().length() * 2 * PI) * (rotations_per_sec)
 	
 	# at a right angle to the vector from player to ball
