f5beb8e3b41543881e5e921252e863e2f13afb89
mdrgon1 <mdrgon1@umbc.edu>
2020-06-15 22:30:34 -0400

    creates level templates and basic camera movement
    3 level templates all work fine, coolio, camera moves up with player

---
+++ Project/Scripts/Camera.gd
@@ -0,0 +1,8 @@
+extends Camera2D
+
+const Y_OFFSET = 0
+
+onready var player = get_tree().get_nodes_in_group("player")[0]
+
+func _process(delta):
+	position.y = min(position.y, player.position.y + Y_OFFSET)
--- Project/Scripts/MainLevel.gd
+++ Project/Scripts/MainLevel.gd
@@ -1,14 +1,15 @@
 extends Node
 
-var num_levels = 1
+var num_levels = 3
 
 func _process(delta):
 	if(Input.is_action_just_pressed("throw_ball")):
-		create_level(Vector2(0, 0))
+		create_level(Vector2(randi() % 20, randi() % 20))
 
 # create a random level at a given position
 func create_level(position : Vector2):
-	var level_index = randi() % num_levels
+	var level_index = rand_seed(OS.get_ticks_msec())[0] % num_levels # seed level generation with time
+	level_index *= sign(level_index) # make sure the index is positive
 	
 	# load a random level and instance it
 	var level_path = "res://level_templates/Template" + String(level_index) + ".tscn"
