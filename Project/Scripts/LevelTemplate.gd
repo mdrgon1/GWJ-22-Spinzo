aef08599f910b4e1906ea133e1f4b49ae08653d0
mdrgon1 <mdrgon1@umbc.edu>
2020-06-15 22:44:38 -0400

    basic hazard generation
    created 3 empty hazard scenes that the levels will randomly place into the spawn points

--- Project/Scripts/LevelTemplate.gd
+++ Project/Scripts/LevelTemplate.gd
@@ -1,10 +1,24 @@
 extends Node2D
 
+var num_hazards = 3
+
 func _ready():
-	var enemy_spawns = $EnemySpawns
-	for child in enemy_spawns.get_children():
+	var hazard_spawns = $EnemySpawns
+	for child in hazard_spawns.get_children():
 		create_enemy_at(child)
-		child.call_deferred("queue_free")
 
 func create_enemy_at(target : Node2D):
-	print(target.global_position)
+	var hazard_index = rand_seed(OS.get_ticks_usec())[0] % (num_hazards + 1) # seed hazard generation with time
+	hazard_index *= sign(hazard_index) # make sure the index is positive
+	
+	# theres a chance there will be no hazard
+	if(hazard_index >= num_hazards):
+		return
+	
+	# load a random hazard and instance it
+	var hazard_path = "res://Hazards/Hazard" + String(hazard_index) + ".tscn"
+	var hazard = load(hazard_path).instance()
+	
+	hazard.set_position(target.position)
+	call_deferred("add_child", hazard)
+	target.call_deferred("queue_free")
