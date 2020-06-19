3801acf72b5d1f7a5de47fbfedaed0421c296d56
mdrgon1 <mdrgon1@umbc.edu>
2020-06-19 19:10:29 -0400

    created player outline with animations

--- Project/Player Outline/PlayerOutline.gd
+++ Project/Player Outline/PlayerOutline.gd
@@ -1,4 +1,16 @@
 extends Trailing
 
+onready var sprite = $Sprite
+
+func _ready():
+	sprite.scale = target.sprite.scale
+	sprite.offset = target.sprite.offset
+	if(target in get_tree().get_nodes_in_group("player")):
+		pos_lerp = 60
+
 func _physics_process(delta):
 	run(delta)
+
+func _process(delta):
+	if(sprite.animation != target.sprite.animation):
+		sprite.play(target.sprite.animation)
